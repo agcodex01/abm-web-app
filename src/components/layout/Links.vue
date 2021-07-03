41c71ab1be66d2335bb10addd2f5b55ebf6e89fd
llorem <54927077+loremZyn14@users.noreply.github.com>
2021-07-03 10:54:57 +0800

    ABM-21: Main layout (#10)
    * ABM-4 Add server configuration (#1)
    
    * Abm 4 wep app setup (#2)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 wep app setup (#3)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update dependencies
    
    * ABM-4 wep app setup (#4)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update dependencies
    
    * Add server configuration
    
    * ABM-4 wep app setup (#5)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update dependencies
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 wep app setup (#6)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update dependencies
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update package.json
    
    * ABM-4 wep app setup (#7)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update dependencies
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update package.json
    
    * ABM-4 update package.json
    
    * ABM-4 wep app setup (#8)
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update dependencies
    
    * Add server configuration
    
    * ABM-4 update dependencies
    
    * ABM-4 update package.json
    
    * ABM-4 update package.json
    
    * ABM-4 Update package.json
    
    * ABM-20: Theme color (#9)
    
    * ABM-21: Update setup

---
+++ src/components/layout/Links.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-list bordered separator class="text-white">
+    <q-item class="q-pa-lg">
+      <q-item-section avatar>
+        <q-avatar>
+          <img
+            src="https://www.adopisoft.com/assets/adopisoft-100px-logo-bordered-6a09c3ac6bcf48e4b0418882dd83f088ad8293e1161b168a0e42d2d6638169b7.png"
+          />
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label class="text-weight-bold">ADOPISOFT</q-item-label>
+        <q-item-label caption lines="1" class="text-white"
+          >Billing Machine</q-item-label
+        >
+      </q-item-section>
+    </q-item>
+
+    <link-item v-for="link in links" :key="link.title" v-bind="link" />
+  </q-list>
+</template>
+
+<script>
+import LinkItem from './LinkItem.vue'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Links',
+  components: {
+    LinkItem
+  },
+  computed: {
+    ...mapGetters({
+      links: 'layout/getLinks'
+    })
+  }
+}
+</script>
---
+++ src/store/modules/layout/links.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: 'Dashboard',
+    icon: 'dashboard',
+    link: '/'
+  },
+  {
+    title: 'Units',
+    icon: 'devices',
+    link: '/units'
+  },
+  {
+    title: 'Billers',
+    icon: 'chat',
+    link: '/billers'
+  },
+  {
+    title: 'Transactions',
+    icon: 'tablet',
+    link: '/transactions'
+  },
+  {
+    title: 'Collections',
+    icon: 'money',
+    link: '/collections'
+  },
+  {
+    title: 'Remits',
+    icon: 'public',
+    link: '/remits'
+  },
+  {
+    title: 'Chats',
+    icon: 'chat',
+    link: '/chats'
+  },
+  {
+    title: 'Settings',
+    icon: 'settings',
+    link: '/settings'
+  }
+]

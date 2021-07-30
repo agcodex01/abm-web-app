c297d809022c95a235054391f9cd0014268a1442
loremZyn14 <yasuaimo@gmail.com>
2021-07-30 14:12:42 +0800

    ABM-13: units page

--- src/pages/units/CreateUnit.vue
+++ src/pages/units/CreateUnit.vue
@@ -7,27 +7,52 @@
         <q-breadcrumbs-el label="Create Unit" />
       </q-breadcrumbs>
     </div>
-    <div class="q-ma-lg">
-        <q-p> <strong>Information</strong></q-p>
-        <div class="q-pa-md q-gutter-lg row items-start justify-around">
-            <q-input class="col-5" outlined label="Name"  />
-            <q-input class="col-5" outlined label="Funds"   type="number"/>
+    <div>
+      <h5 class="text-grey-7">Information</h5>
+      <div class="row q-col-gutter-sm">
+        <div class="col col-md-6">
+          <q-input outlined label="Name"/>
         </div>
-        <q-p><strong>Address or Location</strong></q-p>
-        <div class="q-pa-md q-gutter-lg row items-start justify-around">
-            <q-input class="col-3" outlined label="Postal Code"  />
-            <q-input class="col-3" outlined label="Province"  />
-            <q-input class="col-3" outlined label="City"  />
+        <div class="col col-md-6">
+          <q-input outlined label="Funds" type="number" />
         </div>
-        <div class="q-pa-md q-gutter-lg row items-start justify-around">
-            <q-input class="col-3" outlined label="Municipality"  />
-            <q-input class="col-3" outlined label="Barangay"  />
-            <q-input class="col-3" outlined label="Street"  />
+      </div>
+      <h5 class="text-grey-7">Address or Location</h5>
+      <div class="row q-col-gutter-sm">
+        <div class="col col-md-4">
+          <q-input outlined label="Postal Code"/>
         </div>
+        <div class="col col-md-4">
+          <q-input outlined label="Province"/>
+        </div>
+        <div class="col col-md-4">
+          <q-input outlined label="City" />
+        </div>
+         <div class="col col-md-4">
+          <q-input outlined label="Municipality"/>
+        </div>
+        <div class="col col-md-4">
+          <q-input outlined label="Barangay"/>
+        </div>
+        <div class="col col-md-4">
+          <q-input outlined label="Street" />
+        </div>
+      </div>
     </div>
     <div class="absolute-bottom-right q-ma-md">
-        <q-btn align="around" class="q-mr-md" @click="$router.replace('/units/create-unit')" color="negative" label="Reset" />
-        <q-btn align="around" @click="$router.replace('/units/create-unit')" color="primary" label="Save" />
+      <q-btn
+        align="around"
+        class="q-mr-md"
+        @click="$router.replace('/units/create-unit')"
+        color="negative"
+        label="Reset"
+      />
+      <q-btn
+        align="around"
+        @click="$router.replace('/units/create-unit')"
+        color="primary"
+        label="Save"
+      />
     </div>
   </q-page>
 </template>

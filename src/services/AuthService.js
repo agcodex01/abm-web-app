7203652c3c95aa016a74c199cc538540bc9142b3
loremZyn14 <yasuaimo@gmail.com>
2021-09-02 22:25:54 +0800

    update

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,16 +3,9 @@ import { api } from './../boot/axios'
 export default {
   login: (credentials) => {
     return new Promise((resolve, reject) => {
-      api
-        .get('http://localhost:8000/sanctum/csrf-cookie')
-        .then((response) => {
-          api
-            .post('http://localhost:8000/login', credentials)
-            .then((response) => resolve(response))
-            .catch((error) => reject(error))
-        }).catch((error) => {
-          reject(error)
-        })
+      api.post('login', credentials)
+        .then((response) => resolve(response))
+        .catch((error) => reject(error))
     })
   },
   logout: async (id) => {

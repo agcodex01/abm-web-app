b4bc4c905fad6c6278df417b3812349b434f4471
llorem <54927077+loremZyn14@users.noreply.github.com>
2021-09-02 22:08:55 +0800

    ABM-26: update auth (#24)

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,16 +3,9 @@ import { api, API_BASE_URL } from './../boot/axios'
 export default {
   login: (credentials) => {
     return new Promise((resolve, reject) => {
-      api
-        .get(`${API_BASE_URL}/sanctum/csrf-cookie`)
-        .then((response) => {
-          api
-            .post(`${API_BASE_URL}/login`, credentials)
-            .then((response) => resolve(response))
-            .catch((error) => reject(error))
-        }).catch((error) => {
-          reject(error)
-        })
+      api.post(`${API_BASE_URL}/login`, credentials)
+        .then((response) => resolve(response))
+        .catch((error) => reject(error))
     })
   },
   logout: async (id) => {
